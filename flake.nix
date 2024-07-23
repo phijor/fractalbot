ce96eee46d3e75f4b9e2572eb43d6f546239cda8
Philipp Joram <mail@phijor.me>
2024-07-23 23:17:41 +0300

    build(nix): Do not use custom toolchain overlay
    Without the custom overlay, we prevent unnecessary rebuilds of Cargo and
    friends. We're using Rust stable anyways, the crate is not going to
    break.

--- flake.nix
+++ flake.nix
@@ -3,24 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     rust-crate2nix.url = "github:nix-community/crate2nix";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs @ {
     flake-parts,
-    rust-overlay,
     rust-crate2nix,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       imports = [
-        ./nix/overlays.nix
         ./nix/fractalbot.nix
       ];
       perSystem = {
