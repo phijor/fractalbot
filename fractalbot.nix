f30d47964b1c761366f05ad0fd219c5cb1cb5603
Philipp Joram <mail@phijor.me>
2025-06-10 00:37:28 +0300

    release: v0.3.4

--- fractalbot.nix
+++ fractalbot.nix
@@ -5,7 +5,7 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "fractalbot";
-  version = "0.3.3";
+  version = "0.3.4";
   src = lib.cleanSource ./.;
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
