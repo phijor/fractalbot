4af205d23428371d3709beaee0518d8b0e4bcff6
Philipp Joram <mail@phijor.me>
2025-07-21 23:08:00 +0300

    build(nix): Provide `cargo-audit` in dev shell

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               cargo
+              cargo-audit
               cargo-outdated
               clippy
               rustc
