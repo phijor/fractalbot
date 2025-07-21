12aa7b9122a176384f1f4520a8260156782bb953
Philipp Joram <mail@phijor.me>
2025-07-21 23:54:45 +0300

    ci: Use cached builds even if CI scripts change
    Excluding the `.github` directory allows iterating on CI scripts while
    still using cached builds of the Rust code whenever possible.

--- fractalbot.nix
+++ fractalbot.nix
@@ -6,7 +6,10 @@
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "fractalbot";
   version = "0.3.5";
-  src = lib.cleanSource ./.;
+  src = lib.cleanSourceWith {
+    src = ./.;
+    filter = path: type: !(type == "directory" && baseNameOf path == ".github");
+  };
   cargoDeps = rustPlatform.importCargoLock {
     lockFile = ./Cargo.lock;
   };
